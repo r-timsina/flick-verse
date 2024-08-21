5da36adad65add8a065c70b54c0396cac5609c26
Roshan Timsina <info.roshantimsina@gmail.com>
2024-08-21 22:30:07 +0545

    fix: update UI

--- pages/index.vue
+++ pages/index.vue
@@ -8,17 +8,21 @@ const { data, error, pending, refresh } = await useFetch(
 </script>
 <template>
   <div class="bg-secondary h-screen">
-    <form class="w-full py-24 flex items-center justify-center gap-10">
+    <form class="w-full pt-24 flex items-center justify-center gap-10">
       <input
         class="md:w-1/3 h-16 border rounded-sm p-6"
         type="text"
         placeholder="Search a movie"
         v-model="query"
       />
-      <button class="bg-primary border rounded-sm w-24 h-10">Search</button>
+      <button class="bg-primary shadow-lg shadow-primary/50 w-24 h-10">
+        Search
+      </button>
     </form>
-    <div class="w-full flex justify-items-center">
-      <div class="flex flex-col gap-6 md:w-1/3">
+    <div class="w-full flex items-center justify-center gap-10 h-3/4 pt-6">
+      <div
+        class="flex flex-col gap-6 md:w-1/3 p-6 bg-slate-700 h-full overflow-scroll"
+      >
         <ul v-for="movie in data.Search" :key="movie.imdbID">
           <li class="flex">
             <div>
@@ -32,6 +36,7 @@ const { data, error, pending, refresh } = await useFetch(
           </li>
         </ul>
       </div>
+      <div class="w-24"></div>
     </div>
   </div>
 </template>

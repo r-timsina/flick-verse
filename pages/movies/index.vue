b222f05f50d4ca736b41fca793028df779065500
Roshan Timsina <info.roshantimsina@gmail.com>
2024-08-22 18:18:21 +0545

    fix: remove unwanted comments

--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,14 +1,6 @@
 <script setup>
 const query = ref("batman");
 
-// const search = async () => {
-//   const { data, error } = await useFetch(
-//     `http://www.omdbapi.com/?apikey=68d99cc1&s=${query.value}`
-//   );
-//   movies.value = Search;
-//   query.value = "";
-// };
-
 const { data, error, pending, refresh } = await useFetch(
   `http://www.omdbapi.com/`,
   { query: { apikey: "68d99cc1", s: query } }
